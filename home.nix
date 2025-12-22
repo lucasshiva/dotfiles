7ddca15b09ba1805d090ecce8e6b18e1251981d1
Lucas Silva <silva.lucasdev@gmail.com>
2025-12-22 02:03:40 -0300

    enable eza to replace ls
    We also download a nerd font for icons

--- home.nix
+++ home.nix
@@ -17,11 +17,7 @@
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
-  home.packages = [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
-
+  home.packages = with pkgs; [
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
@@ -34,6 +30,7 @@
     # (pkgs.writeShellScriptBin "my-hello" ''
     #   echo "Hello, ${config.home.username}!"
     # '')
+    nerd-fonts.monaspace
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
@@ -114,6 +111,19 @@
     enableZshIntegration = true;
     enableFishIntegration = true;
     settings = lib.importTOML ./starship/starship.toml;
+  };
+
+  programs.eza = {
+    enable = true;
+    enableZshIntegration = true;
+    enableFishIntegration = true;
+    extraOptions = [
+      "--header"
+      "--group-directories-first"
+    ];
+    icons = "auto";  # This requires a nerd font.
+    git = true;
+    colors = "always";
   };
 
   # Maybe we could also manage the settings from here.
