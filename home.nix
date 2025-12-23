2749ce73e0be8ee9e21469a0d75fdb51887ee120
Lucas Silva <silva.lucasdev@gmail.com>
2025-12-23 18:34:52 -0300

    programs: add delta - syntax highlighting for git diff and more

--- home.nix
+++ home.nix
@@ -66,6 +66,7 @@
       user.name = "Lucas Silva";
       user.email = "silva.lucasdev@gmail.com";
       init.defaultBranch = "main";
+      merge.conflictStyle = "zdiff3"; # For `delta`.
     };
   };
 
@@ -168,6 +169,19 @@
   # Fast text searcher. Can be used as a faster `grep` alternative.
   programs.ripgrep = {
     enable = true;
+  };
+
+  # Syntax highlighting for git diffs.
+  programs.delta = {
+    enable = true;
+    enableGitIntegration = true;
+    options = {
+      navigate = true;
+      dark = true;
+      side-by-side = true;
+      line-numbers = true;
+      syntax-theme = "Catppuccin Mocha";
+    };
   };
 
   # Password manager.
