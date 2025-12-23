ed0fc78b63ba77e7fc997b532859b1ba059b985b
Lucas Silva <silva.lucasdev@gmail.com>
2025-12-22 21:31:02 -0300

    allow unfree packages

--- flake.nix
+++ flake.nix
@@ -21,8 +21,13 @@
 
         # Specify your home configuration modules here, for example,
         # the path to your home.nix.
-        modules = [ ./home.nix ];
+        modules = [
+          {
+            nixpkgs.config.allowUnfree = true;
+          }
 
+          ./home.nix
+         ];
         # Optionally use extraSpecialArgs
         # to pass through arguments to home.nix
       };
