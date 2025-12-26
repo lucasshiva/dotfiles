386c0b4d124600c6edda1a0ab164227ecbd042e1
Lucas Silva <silva.lucasdev@gmail.com>
2025-12-26 12:41:29 -0300

    scripts(docker): use AUR package for docker compose

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-sudo pacman -S docker docker-compose --needed --noconfirm
+# Using `docker-compose-bin` from the AUR because it reports the version correctly.
+yay -S docker docker-compose-bin --needed --noconfirm
 systemctl enable --now docker.service
 sudo usermod -aG docker $USER
+echo "Docker installed! You might need to logout or reboot."
