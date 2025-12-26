b939c51aa83e297a1def73440d6546c0f86dab41
Lucas Silva <silva.lucasdev@gmail.com>
2025-12-26 16:11:47 -0300

    niri: remove dms config files from Niri
    These files will change all the time if we switch themes, so I think it's better to keep them out of the repo.

--- niri/default.nix
+++ niri/default.nix
@@ -1,17 +1,10 @@
 { lib, config, ... }:
 let
   niriConfig = builtins.toString ./config.kdl;
-  dmsDir = builtins.toString ./dms;
   mkLink = path: config.lib.file.mkOutOfStoreSymlink path;
 in
 {
-  # Maybe in the future we could toggle which DMS files to symlink.
   xdg.configFile = {
     "niri/config.kdl".source = mkLink niriConfig;
-    "niri/dms/alttab.kdl".source = mkLink "${dmsDir}/alttab.kdl";
-    "niri/dms/binds.kdl".source = mkLink "${dmsDir}/binds.kdl";
-    "niri/dms/colors.kdl".source = mkLink "${dmsDir}/colors.kdl";
-    "niri/dms/layout.kdl".source = mkLink "${dmsDir}/layout.kdl";
-    "niri/dms/wpblur.kdl".source = mkLink "${dmsDir}/wpblur.kdl";
   };
 }
