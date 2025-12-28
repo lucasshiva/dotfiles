b04d9dc2b67fb9b893be91bb0582149017033129
Lucas Silva <silva.lucasdev@gmail.com>
2025-12-28 17:22:05 -0300

    scripts(dms_shell): remove duplicate qt6ct package

--- scripts/dms-shell.sh
+++ scripts/dms-shell.sh
@@ -12,13 +12,13 @@ DMS_PACKAGES=(
     matugen-bin # Dynamic wallpaper-based theming 
     qt6-multimedia # Sound effect support 
     power-profiles-daemon # Set power profile
-    qt6ct # Qt6 application theming
     adw-gtk-theme # For theming GTK applications
 
     # For theming Qt applications. Must set `qt6ct` env var accordingly.
     # There's also qt6ct-kde, which might have a better integration with KDE apps.
     # For more information on theming, see https://danklinux.com/docs/dankmaterialshell/application-themes
     qt6ct
+    qt5ct
 )
 # I just want to make sure that latest quickshell is available before installing DMS.
 yay -S $QUICKSHELL --needed --noconfirm
